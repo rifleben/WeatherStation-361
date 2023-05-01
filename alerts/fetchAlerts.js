054be6fe5f5546ac0874d9a1cbc871331ebaa27a
Jeff Valcher <jvalcher@gmail.com>
2023-04-30 22:23:01 -0500

    Finished req/res server all state alerts

---
+++ alerts/fetchAlerts.js
@@ -0,0 +1,45 @@
+// fetch all weather alerts for state from NWS
+const fetchAllAlerts = async (state) => {
+
+    const url = `https://api.weather.gov/alerts/active?area=${state}`;
+    let stateAlerts = [];
+
+    // fetch alerts
+    let response;
+    try {
+        response = await fetch(url, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        });
+    } catch (err) {
+        console.log(`${err}\n`)
+        return;
+    }
+    if (response.status !== 200) {
+        console.log(`(fetchAllAlerts) Fetch status ${response.status} for URL:`)
+        console.log(`${url}\n`)
+        return response.status
+    }
+
+    // parse alert areas, headlines, descriptions
+    const alertsObject = await response.json();
+    for await (const feature of alertsObject.features) {
+
+        const headline = feature.properties.headline;
+        const description = feature.properties.description;
+        const areas = feature.properties.areaDesc;
+
+        // push alert to stateAlerts array
+        let alert = {}
+        alert['areas'] = areas;
+        alert['headline'] = headline;
+        alert['description'] = description;
+        stateAlerts.push(alert);
+    }
+
+    return stateAlerts;
+}
+
+module.exports = fetchAllAlerts;
